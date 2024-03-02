88322872e5f87b637544ca744a7d66f3330d8f74
Max Maetze <maetze.max@icloud.com>
2024-03-02 17:01:08 +0000

    convert to react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img
+      src="https://i.imgur.com/MK3eW3Am.jpg"
+      alt="Katherine Johnson"
+    />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ui/buttons/RoundedButton.js
@@ -0,0 +1,10 @@
+const RoundedButton = () => {
+    return (
+      <button>
+        ClickMe
+      </button>
+    );
+  };
+  
+  // export default
+  export default RoundedButton;
